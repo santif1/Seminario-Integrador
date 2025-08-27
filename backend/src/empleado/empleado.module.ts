0d3caef1f573474606427d07cd7e08f5c7ec6b82
augustosan-182 <augustosan7@gmail.com>
2025-08-27 16:46:00 -0300

    arreglo empleado

---
+++ backend/src/empleado/empleado.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities';
+import { AuthGuard } from './middlewares/auth.middleware';
+import { UsersController } from './users/users.controller';
+import { RolesModule } from './roles/roles.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { UsersModule } from './users/users.module';
+import { MiddlewaresModule } from './middlewares/middleware.module';
+import { JwtModule } from './jwt/jwt.module';
+import { SeedService } from './users/seed.service';
+import { JwtService } from './jwt/jwt.service';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5434,
+      database: 'users',
+      username: 'postgres',
+      password: 'postgres',
+      synchronize: false,
+      dropSchema: false,
+      entities,
+    }),
+    TypeOrmModule.forFeature(entities),
+    RolesModule,
+    PermissionsModule,
+    UsersModule,
+    MiddlewaresModule,
+    JwtModule,
+
+  ],
+  controllers: [AppController, UsersController],
+  providers: [
+    SeedService, JwtService
+  ]
+})
+export class AppModule {}
