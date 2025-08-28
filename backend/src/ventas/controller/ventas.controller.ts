0adbb17cae500eca6c05a1dd81fc5803295b1a9f
Santiago Falvo <falvosanti@gmail.com>
2025-08-28 16:18:53 -0300

    Empiezo ventas

--- backend/src/ventas/controller/ventas.controller.ts
+++ backend/src/ventas/controller/ventas.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Param } from '@nestjs/common';
+import { VentasService } from '../service/ventas.service';
 
 @Controller('ventas')
-export class VentasController {}
+export class VentasController {
+
+    constructor(
+        private ventasService: VentasService
+    ){}
+
+    @Get()
+    findAll(){
+        return this.ventasService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number){
+        return this.ventasService.findOne(id);
+    }
+
+}
--- backend/src/ventas/service/ventas.service.ts
+++ backend/src/ventas/service/ventas.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class VentasService {}
+export class VentasService {
+
+    async findAll(){
+        return "Todas las ventas";
+    }
+
+    async findOne(id: number){
+        return "venta con id: " + id;
+    }
+}
